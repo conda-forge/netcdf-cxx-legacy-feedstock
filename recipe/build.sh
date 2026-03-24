41427c5eba1b5b9c3b365a39eb354179b7b66ab6
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-03-24 15:21:14 +0100

    Fix the build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+set -ex
+
 if [[ -n "$mpi" && "$mpi" != "nompi" ]]; then
-  export CC=mpicc
-  export CXX=mpicxx
+  export CC="${PREFIX}/bin/mpicc"
+  export CXX="${PREFIX}/bin/mpicxx"
 fi
 
-./configure --prefix=$PREFIX
+./configure \
+  --prefix="$PREFIX" \
+  CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
+  LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 make
 make check
